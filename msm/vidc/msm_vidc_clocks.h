91dbeffbb4a6526423660b4cb3c918e52885568c
Linux Build Service Account <lnxbuild@localhost>
2021-11-29 22:13:37 -0800

    Merge 938387842d14e6bbb4d005af4a178359561b639c on remote branch
    Change-Id: I02d9d6137d336cbd572c9a0a60e9d981d3de7f6d

--- msm/vidc/msm_vidc_clocks.h
+++ msm/vidc/msm_vidc_clocks.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2018-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
  */
 
 #ifndef _MSM_VIDC_CLOCKS_H_
@@ -9,8 +9,8 @@
 
 void msm_clock_data_reset(struct msm_vidc_inst *inst);
 void msm_dcvs_reset(struct msm_vidc_inst *inst);
-int msm_vidc_set_clocks(struct msm_vidc_core *core, u32 sid);
-int msm_comm_vote_bus(struct msm_vidc_inst *inst);
+int msm_vidc_set_clocks(struct msm_vidc_core *core, u32 sid, bool force_reset);
+int msm_comm_vote_bus(struct msm_vidc_inst *inst, bool force_reset);
 int msm_dcvs_try_enable(struct msm_vidc_inst *inst);
 bool res_is_less_than(u32 width, u32 height, u32 ref_width, u32 ref_height);
 bool res_is_greater_than(u32 width, u32 height, u32 ref_width, u32 ref_height);
